282978758148c2eaa4e8ba60cecba60756bdf478
vanessa <vane.busato9@gmail.com>
2025-12-06 11:11:22 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './index.css'
+
+function index() {
+
+
+
+  return (
+    <>
+      <header className='flex-container'>
+        <img src="./img/dc-logo.png" alt="logo" />
+
+        <nav>
+          <ul className='flex-container'>
+            <li><a href="#">CHARACTERS</a></li>
+            <li><a href="#">COMICS</a></li>
+            <li><a href="#">MOVIES</a></li>
+            <li><a href="#">TV</a></li>
+            <li><a href="#">GAMES</a></li>
+            <li><a href="#">COLLECTIBLES</a></li>
+            <li><a href="#">VIDEOS</a></li>
+            <li><a href="#">FANS</a></li>
+            <li><a href="#">NEWS</a></li>
+            <li><a href="#">SHOP</a></li>
+          </ul>
+        </nav>
+      </header>
+
+
+    </>
+  )
+}
+
+export default index
